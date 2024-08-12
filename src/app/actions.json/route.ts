7398ca69c08b3e21da3b1adc95150771cdc0cdff
inline-arc <atharvmore20@gmail.com>
2024-08-12 20:10:11 +0530

    tx

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -6,12 +6,7 @@ export const GET = async () => {
       // map all root level routes to an action
       {
         pathPattern: "/",
-        apiPath: "/api/donate/",
-      },
-      // idempotent rule as the fallback
-      {
-        pathPattern: "/api/actions/**",
-        apiPath: "/api/actions/**",
+        apiPath: "/actions/donate/",
       },
     ],
   };
--- src/app/actions/donate/route.ts
+++ src/app/actions/donate/route.ts
@@ -1,94 +1,91 @@
-import { ActionGetRequest, ActionPostRequest, ACTIONS_CORS_HEADERS } from "@solana/actions";
-import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
-import { describe } from "node:test";
-import { title } from "process";
+import {
+  ACTIONS_CORS_HEADERS, 
+  ActionGetResponse, 
+  ActionPostRequest, 
+  ActionPostResponse, 
+  createPostResponse,
+  serializeTransaction, 
+} from "@solana/actions";
+
+import {
+  Connection, 
+  LAMPORTS_PER_SOL, 
+  PublicKey, 
+  SystemProgram, 
+  Transaction, 
+  clusterApiUrl, 
+} from "@solana/web3.js";
 
 export async function GET(request: Request) {
-  interface ActionGetRequest {
-    icon: string;
-    title: string;
-    description: string;
-    label: string;
-    links: {
-      actions: {
-        label: string;
-        herf: string;
-      }[];
-    };
-  }
-
-  const url = new URL(request.url);
-  const payload : ActionGetRequest = {
-    icon: "lol",
-    title: "Donate ! Support the project",
-    description : "donate to support the project",
-    label: "Donate Now",
-
+  const url = new URL(request.url); 
+  const payload: ActionGetResponse = {
+    
+    icon: "https://i.pinimg.com/564x/9d/9c/d4/9d9cd4fa1d875e4c22851eb55b2e43f7.jpg",
+    title: "Solona Radar Hackathon", 
+    description: "Book your Spot Now limited spots.", 
+    label: "Donate", 
     links: {
       actions: [
         {
-          label: "Donate 0.1 SOL",
-          herf: "${url.href}?amount=0.1",
-        }
-      ]
-    }
+          label: "Donate 0.1 SOL", 
+          href: `${url.href}?amount=0.1`, 
+        },
+      ],
+    },
   };
   return Response.json(payload, {
-    headers: ACTIONS_CORS_HEADERS
+    headers: ACTIONS_CORS_HEADERS, 
   });
 }
 
-//post req
-export const OPTIONS = GET;
+export const OPTIONS = GET; 
 
 export async function POST(request: Request) {
-  const body: { account: string } = await request.json();
-  const url = new URL(request.url);
-  const amount = Number(url.searchParams.get("amount")) || 0.1;
+  const body: ActionPostRequest = await request.json(); 
+  const url = new URL(request.url); 
+  const amount = Number(url.searchParams.get("amount")) || 0.1; 
   let sender;
 
   try {
-    sender = new PublicKey(body.account);
+    sender = new PublicKey(body.account); 
   } catch (error) {
-    return Response.json({
-      error:{
-        message: "Invalid account",
+    return Response.json(
+      {
+        error: {
+          message: "Invalid account", 
+        },
       },
-    },
-    {
-      status: 400,
-      headers: ACTIONS_CORS_HEADERS
-    }
+      {
+        status: 400, 
+        headers: ACTIONS_CORS_HEADERS, 
+      }
     );
-  } 
+  }
 
-  const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+  const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed"); 
 
   const transaction = new Transaction().add(
     SystemProgram.transfer({
-      fromPubkey: sender,
-      toPubkey: new PublicKey("317pLTpVSKwbN9DbAwogFnPggLuQuEDgFSU4vYxifTEY "),
-      lamports: amount * LAMPORTS_PER_SOL,
+      fromPubkey: sender, 
+      toPubkey: new PublicKey("DBEn8Ke3rhdm3ZSVL8MQdouyGQdsFY3LeN9ygr8USMA"), 
+      lamports: amount * LAMPORTS_PER_SOL, 
     })
   );
-    const blockheight = await connection.getLatestBlockhash();
+  transaction.feePayer = sender; 
+  transaction.recentBlockhash = (
+    await connection.getLatestBlockhash()
+  ).blockhash; 
+  transaction.lastValidBlockHeight = (
+    await connection.getLatestBlockhash()
+  ).lastValidBlockHeight; 
 
-    transaction.recentBlockhash = blockheight.blockhash;
-    transaction.lastValidBlockHeight = blockheight.lastValidBlockHeight;
-
-    transaction.feePayer = sender;
-
-    interface ActionPostRequest {
-      transaction: string;
-      message: string;
-      // other properties...
-    }
-    const payload: ActionPostRequest = {
-      transaction: transaction.serialize().toString("base64"),
-      message: "Transaction sent",
-    };
-
-    return Response.json(payload, {
-      headers: ACTIONS_CORS_HEADERS,  
-    });
+  const payload: ActionPostResponse = await createPostResponse({
+    fields: {
+      transaction,
+      message: "Transaction created", 
+    },
+  });
+  return new Response(JSON.stringify(payload), {
+    headers: ACTIONS_CORS_HEADERS, 
+  });
 }
